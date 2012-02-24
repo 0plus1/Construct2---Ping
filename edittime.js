2994c4ea3abbff6cb754f83a6e56de7720e96037
David <david.0plus1@gmail.com>
2012-02-24 01:52:42 +0100

    first commit

---
+++ edittime.js
@@ -0,0 +1,113 @@
+﻿function GetPluginSettings()
+{
+	return {
+		"name":			"Ping",				// as appears in 'insert object' dialog, can be changed as long as "id" stays the same
+		"id":			"ping",				// this is used to identify this plugin and is saved to the project; never change it
+		"version":		"0.1",					// (float in x.y format) Plugin version - C2 shows compatibility warnings based on this
+		"description":	"<Pings google to check if the user is online>",
+		"author":		"<0plus1>",
+		"help url":		"<n\a>",
+		"category":		"General",				// Prefer to re-use existing categories, but you can set anything here
+		"type":			"object",				// either "world" (appears in layout and is drawn), else "object"
+		"flags":		pf_singleglobal
+	};
+};
+
+////////////////////////////////////////
+// Conditions
+
+// AddCondition(id,					// any positive integer to uniquely identify this condition
+//				flags,				// (see docs) cf_none, cf_trigger, cf_fake_trigger, cf_static, cf_not_invertible,
+//									// cf_deprecated, cf_incompatible_with_triggers, cf_looping
+//				list_name,			// appears in event wizard list
+//				category,			// category in event wizard list
+//				display_str,		// as appears in event sheet - use {0}, {1} for parameters and also <b></b>, <i></i>
+//				description,		// appears in event wizard dialog when selected
+//				script_name);		// corresponding runtime function name
+				
+// example				
+//AddNumberParam("http://", "Useless, don't know how to do this directly.");
+AddCondition(0, cf_none, "Has intertubes?", "My category", "User is online", "Check if the user is online", "isOnline");
+AddCondition(1, cf_none, "Has not intertubes?", "My category", "User is not online", "Check if the user is offline", "isOffline");
+
+////////////////////////////////////////
+ACESDone();
+
+////////////////////////////////////////
+// Array of property grid properties for this plugin
+// new cr.Property(ept_integer,		name,	initial_value,	description)		// an integer value
+// new cr.Property(ept_float,		name,	initial_value,	description)		// a float value
+// new cr.Property(ept_text,		name,	initial_value,	description)		// a string
+// new cr.Property(ept_color,		name,	initial_value,	description)		// a color dropdown
+// new cr.Property(ept_font,		name,	"Arial,-16", 	description)		// a font with the given face name and size
+// new cr.Property(ept_combo,		name,	"Item 1",		description, "Item 1|Item 2|Item 3")	// a dropdown list (initial_value is string of initially selected item)
+// new cr.Property(ept_link,		name,	link_text,		description, "firstonly")		// has no associated value; simply calls "OnPropertyChanged" on click
+
+var property_list = [];
+	
+// Called by IDE when a new object type is to be created
+function CreateIDEObjectType()
+{
+	return new IDEObjectType();
+}
+
+// Class representing an object type in the IDE
+function IDEObjectType()
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+}
+
+// Called by IDE when a new object instance of this type is to be created
+IDEObjectType.prototype.CreateInstance = function(instance)
+{
+	return new IDEInstance(instance);
+}
+
+// Class representing an individual instance of an object in the IDE
+function IDEInstance(instance, type)
+{
+	assert2(this instanceof arguments.callee, "Constructor called as a function");
+	
+	// Save the constructor parameters
+	this.instance = instance;
+	this.type = type;
+	
+	// Set the default property values from the property table
+	this.properties = {};
+	
+	for (var i = 0; i < property_list.length; i++)
+		this.properties[property_list[i].name] = property_list[i].initial_value;
+		
+	// Plugin-specific variables
+	// this.myValue = 0...
+}
+
+// Called when inserted via Insert Object Dialog for the first time
+IDEInstance.prototype.OnInserted = function()
+{
+}
+
+// Called when double clicked in layout
+IDEInstance.prototype.OnDoubleClicked = function()
+{
+}
+
+// Called after a property has been changed in the properties bar
+IDEInstance.prototype.OnPropertyChanged = function(property_name)
+{
+}
+
+// For rendered objects to load fonts or textures
+IDEInstance.prototype.OnRendererInit = function(renderer)
+{
+}
+
+// Called to draw self in the editor if a layout object
+IDEInstance.prototype.Draw = function(renderer)
+{
+}
+
+// For rendered objects to release fonts or textures
+IDEInstance.prototype.OnRendererReleased = function(renderer)
+{
+}
---
+++ runtime.js
@@ -0,0 +1,99 @@
+﻿// ECMAScript 5 strict mode
+"use strict";
+
+assert2(cr, "cr namespace not created");
+assert2(cr.plugins_, "cr.plugins_ not created");
+
+/////////////////////////////////////
+// Plugin class
+// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+//          vvvvvvvv
+cr.plugins_.ping = function(runtime)
+{
+	this.runtime = runtime;
+};
+
+(function ()
+{
+	/////////////////////////////////////
+	// *** CHANGE THE PLUGIN ID HERE *** - must match the "id" property in edittime.js
+	//                            vvvvvvvv
+	var pluginProto = cr.plugins_.ping.prototype;
+		
+	/////////////////////////////////////
+	// Object type class
+	pluginProto.Type = function(plugin)
+	{
+		this.plugin = plugin;
+		this.runtime = plugin.runtime;
+	};
+
+	var typeProto = pluginProto.Type.prototype;
+
+	// called on startup for each object type
+	typeProto.onCreate = function()
+	{
+	};
+
+	/////////////////////////////////////
+	// Instance class
+	pluginProto.Instance = function(type)
+	{
+		this.type = type;
+		this.runtime = type.runtime;
+		
+		// any other properties you need, e.g...
+		// this.myValue = 0;
+	};
+	
+	var instanceProto = pluginProto.Instance.prototype;
+
+	// called whenever an instance is created
+	instanceProto.onCreate = function()
+	{
+		// note the object is sealed after this call; ensure any properties you'll ever need are set on the object
+		// e.g...
+		// this.myValue = 0;
+	};
+	
+	// only called if a layout object - draw to a canvas 2D context
+	instanceProto.draw = function(ctx)
+	{
+	};
+	
+	// only called if a layout object in WebGL mode - draw to the WebGL context
+	// 'glw' is not a WebGL context, it's a wrapper - you can find its methods in GLWrap.js in the install
+	// directory or just copy what other plugins do.
+	instanceProto.drawGL = function (glw)
+	{
+	};
+
+	//////////////////////////////////////
+	// Conditions
+	pluginProto.cnds = {};
+	var cnds = pluginProto.cnds;
+
+	// the example condition
+	cnds.isOnline = function (myparam)
+	{
+		// return true if number is positive
+		return navigator.onLine;
+	};
+	
+	// the example condition
+	cnds.isOffline = function (myparam)
+	{
+		// return true if number is positive
+		return (navigator.onLine)?false:true;
+	};
+	//////////////////////////////////////
+	// Actions
+	pluginProto.acts = {};
+	var acts = pluginProto.acts;
+
+	//////////////////////////////////////
+	// Expressions
+	pluginProto.exps = {};
+	var exps = pluginProto.exps;
+
+}());
